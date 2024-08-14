5ae3908e24cbb0b54d668dcab260f7bfddfed953
2151012001 - Nguyen Van Bin <2151012001bin@ou.edu.vn>
2024-08-14 21:35:30 +0700

    add nav-bar  basic

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,10 @@
 import React from 'react'
-import { IoIosAlarm } from "react-icons/io";
-
-
-import Button from 'react-bootstrap/Button';
-
-function OutlineTypesExample() {
-  return (
-    <div>
-      <Button variant="outline-primary">Primary</Button>
-      <Button variant="outline-secondary">Secondary</Button>
-      <Button variant="outline-success">Success</Button>
-      <Button variant="outline-warning">Warning</Button>
-      <Button variant="outline-danger">Danger</Button>
-      <Button variant="outline-info">Info</Button>
-      <Button variant="outline-light">Light</Button>
-      <Button variant="outline-dark">Dark</Button>
-    </div>
-  );
-}
-
-
+import NavBar from './components/NavigationBar.js'
 
 const App = () => {
   return (
     <>
-      <IoIosAlarm />
-
-      <OutlineTypesExample />
+      <NavBar />
     </>
   )
 }
---
+++ my-app/src/components/NavigationBar.Category.js
@@ -0,0 +1,10 @@
+
+const Category = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Category
