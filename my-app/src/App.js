d2d46e16c28c314544022d8f3a1b71db52247eff
2151012001 - Nguyen Van Bin <2151012001bin@ou.edu.vn>
2024-08-14 21:14:56 +0700

    test React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { IoIosAlarm } from "react-icons/io";
 
-function App() {
+
+import Button from 'react-bootstrap/Button';
+
+function OutlineTypesExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="outline-primary">Primary</Button>
+      <Button variant="outline-secondary">Secondary</Button>
+      <Button variant="outline-success">Success</Button>
+      <Button variant="outline-warning">Warning</Button>
+      <Button variant="outline-danger">Danger</Button>
+      <Button variant="outline-info">Info</Button>
+      <Button variant="outline-light">Light</Button>
+      <Button variant="outline-dark">Dark</Button>
     </div>
   );
 }
 
-export default App;
+
+
+const App = () => {
+  return (
+    <>
+      <IoIosAlarm />
+
+      <OutlineTypesExample />
+    </>
+  )
+}
+
+export default App
