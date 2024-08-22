b1347529e1878fa031ad4d27cfbde950fedc58e4
2151012001 - Nguyen Van Bin <2151012001bin@ou.edu.vn>
2024-08-22 20:49:20 +0700

    add footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,14 +6,17 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import  Product from './components/Product'
 import  Banner from './components/Banner'
 import  ProductType from './components/ProductType'
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <Router>
       <Nav />
+
       <ProductType />
       <Product />
 
+      <Footer />
     </ Router>
   
   )
