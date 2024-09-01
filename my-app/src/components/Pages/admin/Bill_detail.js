8396d1738ecefd6a54203115b90578b9bc363bee
2151012001 - Nguyen Van Bin <2151012001bin@ou.edu.vn>
2024-09-01 19:02:54 +0700

    finish Bill_detail.js for admin page

--- my-app/src/components/Pages/admin/Bill_detail.js
+++ my-app/src/components/Pages/admin/Bill_detail.js
@@ -16,11 +16,81 @@ export default function Bill_detail() {
     }
 
     return (
-        <div>
-            <h1>Bill Detail</h1>
-            <p>Bill ID: {billData.id}</p>
-            <p>Amount: ${billData.amount}</p>
-            <p>Date: {billData.date}</p>
+        <div className="bill-cont">
+            <div className="title-cont">
+                <div className="title">Bill</div>
+                <div className="order-code">Oder code: 31234123123</div>
+            </div>
+            <div className="billDetail-cont">
+                <div className="info-item">
+                    <div className="item">
+                        <div className="img-cont">
+                            <img src={require('../../../resources/Phone/iphone-15-plus_1__1.webp')} alt="" />
+                        </div>
+                        <div className="item-info">
+                            <div className="item-name">Iphone 15 Plus</div>
+                            <div className="item-price">Price: 20.000.000</div>
+                            <div className="item-quantity">Quantity: 2</div>
+                            <div className="item-total">
+                                <div>Total:</div>
+                                <div>40.000.000</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <div className="img-cont">
+                            <img src={require('../../../resources/Phone/iphone-15-plus_1__1.webp')} alt="" />
+                        </div>
+                        <div className="item-info">
+                            <div className="item-name">Iphone 15 Plus</div>
+                            <div className="item-price">Price: 20.000.000</div>
+                            <div className="item-quantity">Quantity: 2</div>
+                            <div className="item-total">
+                                <div>Total:</div>
+                                <div>40.000.000</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="total-price">
+                        <div className="total-price-text">Total price</div>
+                        <div className="total-price-value">20.000.000</div>
+                    </div>
+                </div>
+                <div className="customer-shipping-info">
+                    <div className="customer-info">
+                        <div className="info-item item">
+                            Họ và tên: <strong>QTV</strong>
+                        </div>
+                        <div className="info-item item">
+                            Địa chỉ: <strong>Xã Hùng Sơn, Huyện Hiệp Hòa, Tỉnh Bắc Giang</strong>
+                        </div>
+                        <div className="info-item item">
+                            Địa chỉ giao hàng: <strong>Hẻm 51</strong>
+                        </div>
+                        <div className="info-item item">
+                            Số điện thoại: <strong>0336216546</strong>
+                        </div>
+                        <div className="info-item item">
+                            Ghi chú: <strong>gói hàng kĩ</strong>
+                        </div>
+                    </div>
+                    <div className="shipping-info">
+                        <div className="order-info">
+                            <div>Mã đơn hàng: 2024072806155766A5E21D4F2E5</div>
+                            <div>
+                                Trạng thái đơn hàng: <span className="status">pending</span>
+                            </div>
+                            <div>Ngày: 28/07/2024</div>
+                            <div>Email: admin@gmail.com</div>
+                            <div>Tổng cộng: 58.180.000₫</div>
+                            <div>
+                                Phương thức thanh toán:{' '}
+                                <span className="payment-method">Trả tiền mặt khi nhận hàng</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
